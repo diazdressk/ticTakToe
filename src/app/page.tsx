49b96501ee7d9caabcf498771a40090e61e76aca
Adilet Abdurasulov <a.abdurasulov@catering.rrs>
2024-11-24 00:34:53 +0600

    backend part setting

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,22 @@
+import { prisma } from "@/shared/lib/db";
 import { Button } from "@/shared/ui/button";
+import { Card, CardTitle } from "@/shared/ui/card";
 
-export default function Home() {
+export default async function Home() {
+  const games = await prisma.game.findMany();
+
+  console.log(games, "gggggggggggg");
+
   return (
     <div>
-      <Button size="lg" variant="secondary">Hiii</Button>
+      <Button size="lg" variant="secondary">
+        Hiii
+      </Button>
+      {games.map((game) => (
+        <Card key={game.id}>
+          <CardTitle>{game.name}</CardTitle>
+        </Card>
+      ))}
     </div>
   );
 }
